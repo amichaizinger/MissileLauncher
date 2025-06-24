1158242da315839275473475a707daf9010ad941
amichaizinger <zinger.amichai@gmal.com>
2025-06-25 00:27:25 +0300

    first commit

---
+++ MissileLauncher/AddMissilesCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuBuilder;
+
+namespace MissileLauncher
+{
+    public class AddMissilesCommand : IMenuCommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Which Missile would you want to add? the options are: Torpedo, Balistic, Cruise");
+            string missileTypeInput = Console.ReadLine();
+
+            if (Enum.TryParse(missileTypeInput.ToLower(), true, out MissleType missileType))
+            {
+                Console.WriteLine("how many would you want to add?");
+                string quantityInput = Console.ReadLine();
+                if (int.TryParse(quantityInput, out int quantity) && quantity > 0)
+                {
+                    Console.WriteLine($"Adding {quantity} {missileType} missiles to the inventory.");
+                    // Here you would add the logic to actually add the missiles to your inventory.
+                }
+                else
+                {
+                    Console.WriteLine("Invalid quantity. Please enter a positive integer.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid missile type. Please try again.");
+            }
+
+        }
+    }
+}
---
+++ MissileLauncher/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MenuBuilder;
+
+IMenuInputHandler inputHandler = new NumberInputHandler();
+IMenu mainMenu = new MainMenu(inputHandler);
+
+
+mainMenu.Commands.Add("Store new missile/s", new DefaultCommand());
+mainMenu.Commands.Add("Launch Missiles", new DefaultCommand());
+mainMenu.Commands.Add("Inventory report", new DefaultCommand());
+mainMenu.Commands.Add("Clean out missiles", new DefaultCommand());
+mainMenu.Commands.Add("Shutdown the S6ML", new DefaultCommand());
+
+
+mainMenu.showMenu();
